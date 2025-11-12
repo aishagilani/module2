8095cf3af56c704b7a1a130d10e0cf227595a2f0
aisha gilani <aishaagilani@gmail.com>
2025-11-12 17:58:27 +0530

    Create Operators.java
    Signed-off-by: aisha gilani <aishaagilani@gmail.com>

---
+++ Operators.java
@@ -0,0 +1,11 @@
+public class Operators {
+    public static void main(String[] args) {
+        int num = 10;
+        num += 5;
+        System.out.println("After += operation: " + num);
+        num -= 3;
+        System.out.println("After -= operation: " + num);
+        num *= 2;
+        System.out.println("After *= operation: " + num);
+    }
+}
